18ab68c053df0866bab990b410c44e424a1024ab
Michieli Mattia <mattia.michieli@cz.lan>
2025-04-01 13:06:10 +0200

    Prima gestione tramite cartelle dei file

---
+++ codice/Dati.java
@@ -0,0 +1,12 @@
+package codice;
+
+
+public class Dati {
+    private String comune;
+    private String provincia;
+    private String tipologia;
+    private int stelle;
+    private String nome;
+    private int telefono;
+    private 
+}
---
+++ codice/LettoreCSVfile.java
@@ -0,0 +1,5 @@
+package codice;
+
+public class LettoreCSVfile {
+
+}
